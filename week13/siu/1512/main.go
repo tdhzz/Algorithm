65f0e88a0d47d5bfd25697e786f061fc77bf7917
Jooseph <1092113110@qq.com>
2020-12-05 16:21:30 +0800

    feat: siu's week13 solutions #2

--- week13/siu/1512/main.go
+++ week13/siu/1512/main.go
@@ -1,1 +1,20 @@
 package main
+
+// 一个数在数组的好数对个数：count * (count - 1) / 2
+
+func numIdenticalPairs(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	numMap := make(map[int]int)
+	for _, v := range nums {
+		numMap[v]++
+	}
+
+	count := 0
+	for _, numCount := range numMap {
+		count += numCount * (numCount - 1) / 2
+	}
+	return count
+}
