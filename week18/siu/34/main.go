219ff5b61055a95c70b9dadffe096330f507df0c
Jooseph <1092113110@qq.com>
2021-01-10 16:15:17 +0800

    feat: siu's week18 solutions

---
+++ week18/siu/34/main.go
@@ -0,0 +1,89 @@
+package main
+
+import "fmt"
+
+//给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+//
+// 如果数组中不存在目标值 target，返回 [-1, -1]。
+//
+// 进阶：
+//
+//
+// 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [5,7,7,8,8,10], target = 8
+//输出：[3,4]
+//
+// 示例 2：
+//
+//
+//输入：nums = [5,7,7,8,8,10], target = 6
+//输出：[-1,-1]
+//
+// 示例 3：
+//
+//
+//输入：nums = [], target = 0
+//输出：[-1,-1]
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+// nums 是一个非递减数组
+// -109 <= target <= 109
+//
+// Related Topics 数组 二分查找
+// 👍 799 👎 0
+
+func main() {
+	nums := []int{5, 7, 7, 8, 8, 10}
+	target := 8
+	fmt.Println(searchRange(nums, target))
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func searchRange(nums []int, target int) []int {
+	lower := lowerBound(nums, target)
+	if lower == len(nums) || nums[lower] != target {
+		return []int{-1, -1}
+	}
+	return []int{lower, upperBound(nums, target)}
+}
+
+func lowerBound(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] >= target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
+func upperBound(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] <= target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return right
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
