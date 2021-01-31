a0cc2023f2689755fa3d26a3d51ef807e53ad1f7
fei <1575826619@qq.com>
2021-01-31 21:14:11 +0800

    aiden commit

---
+++ week21/Aiden/1160.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        chars_cnt = collections.Counter(chars)
+        ans = 0
+        for word in words:
+            word_cnt = collections.Counter(word)
+            for c in word_cnt:
+                if chars_cnt[c] < word_cnt[c]:
+                    break
+            else:
+                ans += len(word)
+        return ans
