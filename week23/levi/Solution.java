7a91ce55d1009ca68f3a6a197f6978ea09bcf3f8
levi <728861445@qq.com>
2021-03-21 19:31:39 +0800

    Update Solution.java

--- week23/levi/Solution.java
+++ week23/levi/Solution.java
@@ -1,1 +1,77 @@
 
+public int romanToInt(String s) {
+        int sum = 0;
+        int preNum = getValue(s.charAt(0));
+        for(int i = 1;i < s.length(); i ++) {
+            int num = getValue(s.charAt(i));
+            if(preNum < num) {
+                sum -= preNum;
+            } else {
+                sum += preNum;
+            }
+            preNum = num;
+        }
+        sum += preNum;
+        return sum;
+    }
+    
+    private int getValue(char ch) {
+        switch(ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
+
+public String tree2str(TreeNode t) {
+        if(t==null)
+            return "";
+        if(t.left==null && t.right==null)
+            return t.val+"";
+        if(t.right==null)
+            return t.val+"("+tree2str(t.left)+")";
+        return t.val+"("+tree2str(t.left)+")("+tree2str(t.right)+")";   
+    }
+
+public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        int length = moves.length();
+        for (int i = 0; i < length; i++) {
+            char move = moves.charAt(i);
+            if (move == 'U') {
+                y--;
+            } else if (move == 'D') {
+                y++;
+            } else if (move == 'L') {
+                x--;
+            } else if (move == 'R') {
+                x++;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+ public int removePalindromeSub(String s) {
+        if ("".equals(s)) return 0;
+        if (s.equals(new StringBuilder(s).reverse().toString())) return 1;
+        return 2;
+    }
+
+ public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
