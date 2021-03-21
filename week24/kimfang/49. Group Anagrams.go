b45de2895e20bda4e9e5209b67e672f3266fb390
kimfang(方钦涛) <kimfang@tencent.com>
2021-03-21 19:49:27 +0800

    kimfang

---
+++ week24/kimfang/49. Group Anagrams.go
@@ -0,0 +1,47 @@
+package kimfang
+
+import "sort"
+
+// 49. 字母异位词分组
+//给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+//
+//示例:
+//
+//输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出:
+//[
+//  ["ate","eat","tea"],
+//  ["nat","tan"],
+//  ["bat"]
+//]
+//说明：
+//
+//所有输入均为小写字母。
+//不考虑答案输出的顺序。
+
+type bytes []byte
+func (b bytes) Len() int {
+	return len(b)
+}
+func (b bytes) Less(i,j int) bool {
+	return b[i] < b[j]
+}
+func (b bytes) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
+func groupAnagrams(strs []string) [][]string {
+	ret := [][]string{}
+	m := make(map[string]int)
+	for _, str := range strs {
+		kByte := bytes(str)
+		sort.Sort(kByte)
+		k := string(kByte)
+		if idx, ok := m[k]; !ok {
+			m[k] = len(ret)
+			ret = append(ret, []string{str})
+		} else {
+			ret[idx] = append(ret[idx], str)
+		}
+	}
+	return ret
+}
